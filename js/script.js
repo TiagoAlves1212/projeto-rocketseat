a34a2a0f1111ac40a04a5b9d4080a421e9dcc9dd
TiagoAlves <tiago2018cel123@gmail.com>
2024-08-02 21:44:12 -0300

    site básico sem muitas animações

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const html = document.documentElement
+const img = document.querySelector('img')
+
+toggleMode = () => {
+    html.classList.toggle('light')
+
+    if (html.classList.contains("light")) {
+        img.src = "../img/avatar-light.png"
+    }
+
+    else {
+        img.src = "../img/avatar.png"
+    }
+}
